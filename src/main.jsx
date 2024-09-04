6374c91c0b5e816a1b824006d8377455b13d5a2e
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-09-04 12:20:30 +0200

    basenames updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/portfolio'>
+    <BrowserRouter basename='/'>
       <ThemeProvider theme={theme}>
         <AppRouter />
       </ThemeProvider>
